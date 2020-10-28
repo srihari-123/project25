b5f87f277f0b7518599c29d8d56df7b171f47bce
srihari-123 <72793088+srihari-123@users.noreply.github.com>
2020-10-28 16:36:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var dustbinObj;
+var ground;
+var imgs;
+var ball;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	imgs = loadImage("keep india.jpg")
+}
+
+function setup() {
+  rectMode(CENTER);
+	createCanvas(1200, 600);
+
+  engine = Engine.create();
+	world = engine.world;
+
+  ground = new Ground(600,570,1200,20);
+  dustbinObj = new Dustbin(1000,550);
+  ball=new  PaperBall(80,450);
+  
+	Engine.run(engine);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(imgs);
+  Engine.update(engine);
+ 
+  ground.display();
+  dustbinObj.display();
+  ball.display();
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+if (keyCode=== UP_ARROW) {
+  Matter.Body.applyForce(ball.body,ball.body.position,{x:190,y:-195});
+}
+}
+
